e9196985794c5d6bbd9cebf75e9d8bda6d6a563e
山下拓 <yamashitataku@taku.local>
2020-11-09 15:08:03 +0900

    create flatlist

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -5,41 +5,44 @@ import { List, TextInput } from 'react-native-paper';
 const DATA = [
   {
     id: '1',
-    title: 'First Item',
+    title: '抗酸化物質とは、抗酸化剤とも呼ばれ、生体内、食品、日用品、工業原料において酸素が関与する有害な反応を減弱もしくは除去する物質の総称である。',
+    icon: 'calendar',
   },
   {
     id: '2',
-    title: 'Second Item',
+    title: 'Webサイトのコンテンツ管理システム(CMS)の種類の一つで、簡便な記法を用いて文書の整形や装飾が可能なもの。',
+    icon: '',
   },
   {
     id: '3',
-    title: 'Third Item',
+    title: 'Truncate Title text such that the total number of lines does not exceed this number.',
+    icon: 'clock',
   },
 ];
 
-const Item = ({ title }) => (
-  <List.Section>
-    <List.Item
-      style={styles.listItem}
-      title={title}
-      left={props => <List.Icon {...props} icon="calendar" />}
-    />
-  </List.Section>
+const Items = ({ title, icon }) => (
+  <List.Item
+    style={styles.listItem}
+    title={title}
+    titleNumberOfLines={2}
+    left={props => <List.Icon {...props} icon={icon} />}
+  />
 );
 
 export default function HomeScreen() {
   const [text, setText] = React.useState('');
 
   const renderItem = ({ item }) => (
-    <Item title={item.title} />
+    <Items title={item.title} icon={item.icon} />
   );
 
   return (
     <SafeAreaView style={styles.container}>
       <TextInput
-        placeholder="write task"
+        placeholder="タスクを追加"
         value={text}
         onChangeText={text => setText(text)}
+        left={<TextInput.Icon icon="plus-circle" />}
       />
       <FlatList data={DATA} renderItem={renderItem} keyExtractor={item => item.id} />
     </SafeAreaView>
@@ -53,5 +56,7 @@ const styles = StyleSheet.create({
   },
   listItem: {
     borderBottomWidth: 1,
+    paddingTop: 20,
+    paddingBottom: 20,
   },
 });
